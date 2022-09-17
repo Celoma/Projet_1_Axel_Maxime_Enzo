a45789b5a34db951a996c4148664551aa177b4bd
MaxiLan <103132181+MaxiLan@users.noreply.github.com>
2022-09-17 19:43:50 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import recup_fichier 
+import recup_fichier, extrac
 
 
 annee = input("De quelle année voulez-vous afficher les donnés météorologiques ? (en chiffre)")
@@ -7,3 +7,5 @@ mois = input("Et de quelle mois ? (en chiffre)")
 url = f"https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Synop/Archive/synop.{annee}{mois}.csv.gz"
 
 recup_fichier.recup_fichier(url)
+extrac.extraction_csv(f'synop.{annee}{mois}.csv.gz')
+
