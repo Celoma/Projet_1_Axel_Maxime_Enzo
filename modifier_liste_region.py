ead57cbd20baa888d3161ec7dc98a110c6bdb8d7
parlegentil <112904455+parlegentil@users.noreply.github.com>
2022-09-18 21:35:23 +0200

    Add files via upload

---
+++ modifier_liste_region.py
@@ -0,0 +1,47 @@
+import csv
+
+
+def import_table_dicos(fichier):
+    f = open(fichier, "r", encoding="utf-8")
+    tab_dico = []
+    lignes = f.readlines()
+    f.close()
+    descripteurs = lignes[0].strip().split(";")
+    for i in range(1, len(lignes)):
+        dico = {}
+        valeurs = lignes[i].strip().split(";")
+        for j in range(len(valeurs)):
+            dico[descripteurs[j]] = valeurs[j]
+        tab_dico.append(dico)
+    return tab_dico
+
+
+
+def ajuster_coor(dico):
+    for i in range(len(tab)):
+        if tab[i]["Latitude"][-1] == "N" :
+            eff_dir = "N"
+            tab[i]["Latitude"] = tab[i]["Latitude"].rstrip(eff_dir)
+        else :
+            eff_dir = "S"
+            tab[i]["Latitude"] = "-" + tab[i]["Latitude"].rstrip(eff_dir)
+        if tab[i]["Longitude"][-1] == "E" :
+            eff_dir = "E"
+            tab[i]["Longitude"] = tab[i]["Longitude"].rstrip(eff_dir)
+        else :
+            eff_dir = "W"
+            tab[i]["Longitude"] = "-" + tab[i]["Longitude"].rstrip(eff_dir)
+    return tab
+
+tab = import_table_dicos("region_coord.csv")
+
+true_region_coord = ajuster_coor(tab)
+labels = ['Endroits', 'Latitude', 'Longitude']
+try:
+    with open('true_region_coord.csv', 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=labels)
+        writer.writeheader()
+        for elem in true_region_coord:
+            writer.writerow(elem)
+except IOError:
+    print("I/O error")
